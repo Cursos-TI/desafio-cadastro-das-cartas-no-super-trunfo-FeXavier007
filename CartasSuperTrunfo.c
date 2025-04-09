4e5575540d873bb7b08c3bb467ed85b33c654efd
FELIPE EUGÊNIO XAVIER <felipeudmy@gmail.com>
2025-04-08 22:08:51 -0300

    Teste pirtf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,8 +9,16 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    // nas variáveis c1 = carta 1 e c2 = carta 2
+    char estado_c1, estado_c2;
+    char cidade_c1 [10];
+    char cidade_c2 [10];
+    char codido_c1 [3] = cidade_c1+"01";
+    char codido_c2 [3] = cidade_c2+"02";
+    int populacao_c1, populacao_c2, p_turistico_c1, p_turistico_c2;
+    float area_c1, area_c2, pib_c1, pib_c2;
     // Cadastro das Cartas:
+    printf ("%s \n %s\n",cidade_c1,cidade_c1);
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
